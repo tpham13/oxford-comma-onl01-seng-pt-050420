eeb0f31915a4bfbab7e212bd903d3f32799deea1
Thanh Pham <tpham0123@gmail.com>
2020-05-12 17:48:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,3 +12,10 @@ def oxford_comma(array)
 
 
 end
+
+
+ ## comment 1: array[-1] = "and " + array[-1] 
+ #This calls for the last element of the array and turn
+ #"and" + array[-1] into the same element
+ ##comment 2: array.join(", ") is on line 6 bc 
+ # without it it will involke the last line (line5)
