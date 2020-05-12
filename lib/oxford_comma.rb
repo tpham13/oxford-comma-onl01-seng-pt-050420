6877e02bb07ab6041caa1b6483b40746383c54f7
Thanh Pham <tpham0123@gmail.com>
2020-05-12 17:45:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.length == 2 
+     array.join(" and ")
+  elsif array.length > 2 
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+
+  else array.join
+  
+  end 
+  
+
 
 end
